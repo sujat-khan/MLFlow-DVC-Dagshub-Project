9db1eb9afc9a889f709e48e82129d448be9bee2f
sujat-khan <sujatkhan24@gmail.com>
2026-08-29 22:20:04 +0530

    added token based auth for Dagshub

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -10,9 +10,20 @@ import dagshub
 
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/sujat-khan/MLFlow-DVC-Dagshub-Project.mlflow')
-dagshub.init(repo_owner='sujat-khan', repo_name='MLFlow-DVC-Dagshub-Project', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/sujat-khan/MLFlow-DVC-Dagshub-Project.mlflow')
+# dagshub.init(repo_owner='sujat-khan', repo_name='MLFlow-DVC-Dagshub-Project', mlflow=True)
 
+# Set up DagsHub credentials for MLflow tracking, to be used when Dagshub token is used and save in github repo env
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "sujat-khan"
+repo_name = "MLFlow-DVC-Dagshub-Project"
 
 
 # logging configuration
